ea8af1b02fc45b66bd380dd0b2b10035752087dc
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-27 22:56:31 +0300

    Add articles route

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_sources
+from .request import get_articles, get_sources
 
 # Views
 @app.route('/')
@@ -34,3 +34,15 @@ def index():
 
 
     return render_template('index.html', title = title, business = business_sources, entertainment = entertainment_sources, general = general_sources, health = health_sources, science = science_sources, sports = sports_sources, technology = technology_sources)
+
+
+@app.route('/source/<id>')
+def articles(id):
+    '''
+    View articles page function that returns the articles page that displays articles from a given source
+    '''
+
+    articles = get_articles(id)
+    title = f'Daily Planet | {id}'
+
+    return render_template('articles.html', articles = articles, title = title)
