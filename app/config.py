8e2bf7f3b9db8374a0ad576e5fa66a9836b02973
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-27 14:50:46 +0300

    Add base URLs

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?category={}&language=en&apiKey={}'
+    NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
 
 
 
