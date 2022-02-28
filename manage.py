96fc90fe27aeb58bd59ec00496c3fb96ab3076f6
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-28 19:29:30 +0300

    Heroku deployment 3

--- manage.py
+++ manage.py
@@ -4,10 +4,10 @@ from flask_script import Manager, Server
 # Creating app instance
 app = create_app('production')
 
-manager = Manager(app)
-manager.add_command('server',Server)
+manage = Manager(app)
+manage.add_command('server',Server)
 
-@manager.command
+@manage.command
 def test():
     """Run the unit tests."""
     import unittest
@@ -15,4 +15,4 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
-    manager.run()
+    manage.run()
